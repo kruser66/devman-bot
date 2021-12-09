82e6253d370f68a3ea1692cca71c47f1d6b69a05
kruser <kruser@yandex.ru>
2021-12-09 20:34:54 +0500

    Добавил отправку сообщения в телеграм

--- devman_bot.py
+++ devman_bot.py
@@ -1,5 +1,6 @@
 import os
 import requests
+import telegram
 from time import sleep
 from dotenv import load_dotenv
 from pprint import pprint
@@ -13,42 +14,69 @@ def fetch_user_reviews(token):
     }
 
     response = requests.get(url, headers=headers)
+    response.raise_for_status()
 
-    if response.ok:
-        return response.json()
+    return response.json()
 
 
-def fetch_long_pulling(token):
+def bot_send_messages(chat_id, json):
+
+    for attempt in json['new_attempts']:
+        title = 'У Вас проверили работу:\n"{}"'.format(attempt['lesson_title'])
+        if attempt['is_negative']:
+            correct = '_К сожалению, в работе нашлись ошибки_'
+        else:
+            correct = '''
+            _Преподавателю все понравилось.
+            Можете приступать к следующему заданию_
+            '''
+        link = '[Ссылка на Вашу работу]({})'.format(attempt['lesson_url'])
+        bot.send_message(
+            chat_id=chat_id,
+            text=f'{title}\n\n{link}\n\n{correct}',
+            parse_mode=telegram.ParseMode.MARKDOWN_V2
+        )
+
+
+def long_pulling(token):
     url = 'https://dvmn.org/api/long_polling/'
 
     headers = {
         'Authorization': token,
     }
-
     params = {
         'timestamp': '',
     }
-    while True:
-        try:
-            response = requests.get(url, headers=headers, params=params)
-            answer = response.json()
-            pprint(answer)
-            if answer['status'] == 'timeout':
-                params['timestamp'] = answer['timestamp_to_request']
-        
-        except requests.exceptions.ReadTimeout:
-            print('LongPull Timeout')
-            sleep(15)
 
-        except requests.exceptions.ConnectionError:
-            print('Connection Error')
-            sleep(60)
-  
+    response = requests.get(url, headers=headers, params=params)
+    response.raise_for_status()
+
+    data = response.json()
+    pprint(data)
+    if data['status'] == 'timeout':
+        params['timestamp'] = data['timestamp_to_request']
+    elif data['status'] == 'found':
+        bot_send_messages(tg_chat_id, data)
+        params['timestamp'] = data['timestamp_to_request']
+
 
 if __name__ == '__main__':
 
     load_dotenv()
-    token = os.getenv('DEVMAN_API_TOKEN')
+    devman_api_token = os.getenv('DEVMAN_API_TOKEN')
+    tg_bot_token = os.getenv('TELEGRAM_BOT_TOKEN')
+    tg_chat_id = os.getenv('TG_CHAT_ID_FOR_MESSAGE')
 
-    # pprint(fetch_user_reviews(token))
-    fetch_long_pulling(token)
+    bot = telegram.Bot(token=tg_bot_token)
+
+    # pprint(fetch_user_reviews(devman_api_token))
+    while True:
+        try:
+            long_pulling(devman_api_token)
+
+        except requests.exceptions.ReadTimeout:
+            print('LongPull Timeout')
+            sleep(15)
+        except requests.exceptions.ConnectionError:
+            print('Connection Error')
+            sleep(10)
